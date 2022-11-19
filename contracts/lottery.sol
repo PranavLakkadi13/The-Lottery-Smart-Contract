cc047aa7c54a296815a6a1212f9e590f435d1c3b
PranavLakkadi13 <95748382+PranavLakkadi13@users.noreply.github.com>
2022-11-19 18:03:47 +0530

    Updated the Contract

--- contracts/lottery.sol
+++ contracts/lottery.sol
@@ -1,40 +1,51 @@
-// SPDX-License-Identifier: GPL-3.0
+//SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity ^0.8.0;
 
-contract Lottery{
-    address payable[] public players;
-    address public manager;
+contract lottery {
+    address payable[] public players; // creating an array 
+    address payable public manager;
 
-    constructor(){
-        manager = msg.sender;
+    constructor() {
+        manager = payable(msg.sender); // to assign declare the account address of the manager
+    } 
+
+    receive() external payable { //to make the contrac recieve transactions 
+        require(msg.sender != manager, "Manager can't participate");
+        require(msg.value == 0.1 ether); // this condition checks that atleast 0.1ether is sent into the contract
+        // ^^ if the condition is not met then the transaction will revert but the gas till there will be used 
+        players.push(payable(msg.sender)); // to store all the payable addresses in the array of players since the array is for payable addresses
     }
 
-    receive() external payable{
-        require(msg.value == 0.1 ether);
-        players.push(payable(msg.sender));
-    }
-
-    function getBalance() public view returns(uint){
-        require(msg.sender == manager,"You are not the manager");
+    function getBalance() public view returns(uint) { // to get the balance 
+        require(msg.sender == manager); // the condition to chech that only the manager can see the balance 
         return address(this).balance;
     }
-    //Consider this just as a proof of concept do not use it with actual money
-    function random() public view returns(uint){
-        return uint(keccak256(abi.encodePacked(block.difficulty , block.timestamp , players.length)));
+
+    function random() public view returns(uint) { // this is used to create a random number but dont use on real application
+        // this type of random number could be attacked so avoid it on real usecase contract 
+        return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,players.length)));
     }
 
-    function pickWinner() public {
-        require(msg.sender == manager);
-        require(players.length>=3);
-
+    function pickwinner() public {
+        require(msg.sender == manager); // only access to the manager to announce the result
+        require(players.length >= 3); // atleast 3 participants should enter
+        
         uint r = random();
         address payable winner;
+        uint index = r % players.length; 
         
-        uint index = r % players.length;
+        uint num9 = getBalance() / 10; // to get 10% of the balance
+        uint trans_winner = getBalance() - num9; // to get 90% of the balance
+        
+        
         winner = players[index];
+        winner.transfer(trans_winner); // winner of the lottery gets 90% of the prize money 
+        manager.transfer(num9); // manager gets 10% of the prize money 
 
-        winner.transfer(getBalance());
-        players = new address payable[](0); //reseting the lottery
+
+        players = new address payable[](0); //to reset the lottery 
     }
+
+
 }
